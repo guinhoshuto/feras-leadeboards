a388c04ba74628416b7adc6e285483d79dbcd318
Guilherme S <guinho.shuto@gmail.com>
2022-06-25 23:01:28 -0400

    pegando leaderboards e conecta com o banco

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
 
-/* GET home page. */
+require('dotenv').config();
+const mysql = require('mysql2');
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+console.log('Connected to PlanetScale!');
+connection.end();
+
+
+const params = {
+  headers: {
+    "accept": "application/json", 
+    "authorization": "bearer undefined", 
+    "content-type": "application/json"
+  }
+}
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/:user', async function(req, res, next) {
+  const user = req.params.user;
+  try {
+    const userInfo = await axios.get(`https://api.streamelements.com/kappa/v2/channels/${user}`, params)
+    try { 
+      const leaderboard = await axios.get(`https://api.streamelements.com/kappa/v2/points/${userInfo.data._id}/top?limit=1000&offset=0`, params)
+      res.json(leaderboard.data.users)
+    } catch (e) {
+      res.status(500).json({'message': 'deu ruim pra achar o leaderboard' + e})
+    }
+  } catch (e){
+    res.status(500).json({'message': 'deu ruim pra achar o user' + e})
+  }
+})
 
 module.exports = router;
