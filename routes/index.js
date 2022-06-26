c029299f036a024e7054ec15029dbf7acc56ae01
Guilherme S <guinho.shuto@gmail.com>
2022-06-26 00:01:53 -0400

    insert das paradas

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 console.log('Connected to PlanetScale!');
-connection.end();
-
 
 const params = {
   headers: {
@@ -22,13 +20,32 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/:user', async function(req, res, next) {
-  const user = req.params.user;
+router.get('/:channel', async function(req, res, next) {
+  const channel = req.params.channel;
   try {
-    const userInfo = await axios.get(`https://api.streamelements.com/kappa/v2/channels/${user}`, params)
+    const channelInfo = await axios.get(`https://api.streamelements.com/kappa/v2/channels/${channel}`, params)
     try { 
-      const leaderboard = await axios.get(`https://api.streamelements.com/kappa/v2/points/${userInfo.data._id}/top?limit=1000&offset=0`, params)
-      res.json(leaderboard.data.users)
+      const leaderboard = await axios.get(`https://api.streamelements.com/kappa/v2/points/${channelInfo.data._id}/top?limit=1000&offset=0`, params)
+      const users = leaderboard.data.users;
+
+      connection.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        users.forEach(l => {
+          // let sql = "SELECT * FROM ferasLeaderboard"
+          let sql = 
+          `INSERT INTO ferasLeaderboard (user, channel, channelId, points)
+          VALUES ("${l.username}", "${channelInfo.data.username}", "${channelInfo.data._id}", ${l.points}) ON DUPLICATE KEY UPDATE channel = "${channelInfo.data.username}", channelId = "${channelInfo.data._id}", points = ${l.points};`; 
+          console.log(sql);
+          connection.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted", result);
+          });
+        })
+      });
+      connection.end();
+      res.json(users)
+
     } catch (e) {
       res.status(500).json({'message': 'deu ruim pra achar o leaderboard' + e})
     }
