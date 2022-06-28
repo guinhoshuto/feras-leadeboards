bd3a7b1db7cf83ca1989603d6bbf0b82bfd00262
Guilherme S <guinho.shuto@gmail.com>
2022-06-27 20:20:19 -0400

    umas coisa de cors no app

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/dash', dashRouter);
 
