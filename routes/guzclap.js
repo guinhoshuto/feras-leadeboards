0f98be73f78cfa4359d959a6c63ba193fb557b95
Guilherme S <guinho.shuto@gmail.com>
2022-07-22 22:50:59 -0400

    Ajuste no put de att

--- routes/guzclap.js
+++ routes/guzclap.js
@@ -33,10 +33,11 @@ router.post('/twitch/members', async function (req, res, next) {
 
 router.put('/twitch/:att/:member/:n', async function(req, res, next){
     const n = req.params.n;
-    const operator = n < 0 ? '-' : '+'; 
+    const operator = int(n) < 0 ? '-' : '+'; 
 
     const conn = await db.connect()
     const queryUpdateAtt = `UPDATE guzTwitchMembers SET ${req.params.att} = ${req.params.att} ${operator} ${n} WHERE username = '${req.params.member}'`
+    console.log(queryUpdateAtt)
     conn.query(queryUpdateAtt)
     .then(() => res.json({'msg': 'foi'}))
     .catch((e) => res.status(500).json({'e': e}))
